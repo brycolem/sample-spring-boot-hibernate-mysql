03a8cea8403cd87b215f6233cd0c68ead6c792b9
Bryan Patrick Coleman <totalknowledge@gmail.com>
2023-05-02 09:55:16 -0400

    changed to totalknowledge account at github for image repo

--- build_deploy.sh
+++ build_deploy.sh
@@ -4,10 +4,10 @@
 ./gradlew build -x test
 
 # Podman build
-podman build -t localhost:5001/articles-backend:latest .
+podman build -t totalknowledge/articles-backend:latest .
 
 # Podman push
-podman push --tls-verify=false localhost:5001/articles-backend:latest
+podman push --tls-verify=false totalknowledge/articles-backend:latest
 
 # Apply Kubernetes resources
-kubectl apply -f k8s/
+#kubectl apply -f k8s/
